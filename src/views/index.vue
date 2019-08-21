227b655f8077c823ec5a1275dd446eb317468fec
houjingyuan <cikeyujiyuji@gmail.com>
2019-08-21 12:43:02 +0800

    yuan

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,27 +1,45 @@
 <template>
   <div class="container">
-    <div class="header">
-       <van-icon name="service"  />
+    <header>
+      <span>
+        <van-icon name="service" />
+      </span>
+      <div class="search">
+        <van-search placeholder="请输入搜索关键词" v-model="value" />
+      </div>
+      <span>
+        <van-icon name="flag-o" />
+      </span>
+    </header>
 
-          <van-nav-bar
-        left-text="返回"
-         v-model="value"
-          placeholder="请输入搜索关键词"
-          show-action
-          @search="onSearch"
-          @cancel="onCancel"
-      />
-    </div>
 
-
-        <van-swipe :autoplay="3000" indicator-color="blue" :touchable="true" :height="200">
+    <section>
+       <van-swipe :autoplay="3000" indicator-color="blue" :touchable="true" :height="200">
           <van-swipe-item><img src="../img/1.png" alt=""></van-swipe-item>
           <van-swipe-item>2</van-swipe-item>
           <van-swipe-item>3</van-swipe-item>
           <van-swipe-item>4</van-swipe-item>
         </van-swipe>
 
+        <van-grid :column-num="2">
+          <van-grid-item
+            v-for="value in 10"
+            :key="value"
+            icon="photo-o"
+            text="文字"
+          />
+        </van-grid>
 
+    </section>
+
+
+
+
+
+
+
+
+    <footer>
       <van-tabbar v-model="active">
         <van-tabbar-item icon="home-o">首页</van-tabbar-item>
         <van-tabbar-item icon="expand">菜品</van-tabbar-item>
@@ -29,6 +47,7 @@
         <van-tabbar-item icon="shopping-cart">购物车</van-tabbar-item>
         <van-tabbar-item icon="manager">我的</van-tabbar-item>
       </van-tabbar>
+    </footer>
   </div>
 </template>
 
@@ -47,11 +66,44 @@ export default {
     },
     onCancel(){
 
-    }
-  }
+    },
+
+  },
+
 }
 </script>
 
 <style scoped>
+.container{
+  height: 100vh;
+  display: flex;
+  flex-direction: column
+}
 
+li{
+  height: 50px;
+  border-top:80px solid red
+}
+
+header .search{
+  width: 70%;
+  float: left;
+  margin-top: 15px;
+}
+header span{
+  float: left;
+  width: 60px;
+  margin-left: 20px;
+}
+header,footer{
+  height: 100px;
+  line-height: 100px;
+
+
+}
+
+section{
+  flex: 1;
+  overflow: auto;
+}
 </style>
